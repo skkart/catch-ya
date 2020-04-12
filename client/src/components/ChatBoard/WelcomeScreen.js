43084dcd6719cb60b108aa6f85ee99a80b6ef5fb
skkart <skkarthik10@gmail.com>
2020-04-12 16:37:18 +0530

    Background logo and welcome page

---
+++ client/src/components/ChatBoard/WelcomeScreen.js
@@ -0,0 +1,38 @@
+import React from 'react'
+
+export default function WelcomeScreen(props) {
+  return (
+    <div className="welcomeScreen row h-100">
+      <div className="col-sm-12 my-auto mb-4">
+        <h3 className="h3 mb-3 font-weight-normal">Welcome to CatchYa!!!</h3>
+        <div
+          className="image mb-4"
+        />
+        <div
+          className="form-row text-center"
+          style={{
+            marginBottom: '20px;'
+          }}
+        >
+          <div className="col-12">
+            <p>Chat with your friends, family and groups</p>
+          </div>
+        </div>
+
+        <div className="form-row text-center">
+          <div className="col-12">
+            <button
+              className="btn btn-success"
+              title="Get started"
+              data-toggle="modal"
+              data-target="#addFormModel"
+              data-whatever="@mdo"
+            >
+Get Started
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
